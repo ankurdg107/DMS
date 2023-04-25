d45ccffcd7c6a7906120c69b17654aaacd4816ff
AnkurDG <ankurdattatrayg@gmail.com>
2023-04-25 21:12:15 +0530

    as

---
+++ client/src/components/Display.js
@@ -0,0 +1,91 @@
+// import { useState } from "react";
+import "./Display.css";
+// const Display = ({ contract, account }) => {
+//   const [data, setData] = useState("");
+//   const getdata = async () => {
+//     let dataArray;
+//     const Otheraddress = document.querySelector(".address").value;
+//     console.log(Otheraddress)
+
+//     try {
+//       if (Otheraddress) {
+//         dataArray = await contract.display(Otheraddress);
+//         console.log(dataArray);
+//       } else {
+//         dataArray = await contract.display(account);
+//       }
+//     } catch (e) {
+//       alert("You don't have access");
+//     }
+//     const isEmpty = Object.keys(dataArray).length === 0;
+
+//     if (!isEmpty) {
+//       const str = dataArray.toString();
+//       const str_array = str.split(",");
+//       // console.log(str);
+//       // console.log(str_array);
+//       const images = str_array.map((item, i) => {
+//         return (
+//           <a href={item} key={i} target="_blank">
+//             <img
+//               key={i}
+//               src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`}
+//               alt="new"
+//               className="image-list"
+//             ></img>
+//           </a>
+//         );
+//       });
+//       setData(images);
+//     } else {
+//       alert("No image to display");
+//     }
+//   };
+//   return (
+//     <>
+//       <div className="image-list">{data}</div>
+//       <input
+//         type="text"
+//         placeholder="Enter Address"
+//         className="address"
+//       ></input>
+//       <button className="center button" onClick={getdata}>
+//         Get Data
+//       </button>
+//     </>
+//   );
+// };
+// export default Display;
+
+
+import React, { useState } from "react";
+
+function Display(props) {
+  const { contract, account, user } = props;
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      console.log(account);
+      const result = await contract.display(account);
+      console.log("sdf");
+      setData(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h3>User Data:</h3>
+      <button onClick={fetchData}>Fetch Data</button>
+      <ul>
+        {data.map((d, i) => (
+          <li key={i}>{d}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Display;
